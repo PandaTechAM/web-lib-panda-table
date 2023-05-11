0a6bc172f9ea7132b3c1726c7c87e21091785aa1
Samvel Baghdasaryan <samvel.baghdasaryan@pandatech.it>
2023-05-12 02:17:56 +0400

    webpackchanges

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./src/Table/index.tsx",
+  entry: "./index.tsx",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
