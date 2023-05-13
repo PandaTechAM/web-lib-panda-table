5991b90f5efdaf25fbb05e8a53850a9945679253
Samvel Baghdasaryan <samvel.baghdasaryan@pandatech.it>
2023-05-13 21:51:01 +0400

    rollup-config

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,24 @@
 declare module '*.module.css' {
-    const classes: { readonly [key: string]: string };
-    export default classes;
+  const classes: { readonly [key: string]: string }
+  export default classes
 }
-
 declare module '*.module.scss' {
-    const classes: { readonly [key: string]: string };
-    export default classes;
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+declare module '*.css' {
+  const content: any
+  export default content
+}
+declare module '*.scss' {
+  const content: any
+  export default content
+}
+declare module '*.json' {
+  const content: any
+  export default content
 }
