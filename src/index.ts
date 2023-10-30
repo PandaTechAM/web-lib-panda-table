5c7650ed8a79d6683150dbd0765f30ffdc22fd57
Samvel Baghdasaryan <samvel.baghdasaryan@pandatech.it>
2023-10-30 14:56:29 +0400

    types_exporting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
 import Table from './table'
-
-export { Table }
+import {
+    IProps,
+    IColumnTotal,
+    IColumns,
+    IHeaders,
+    ITotals,
+    IColumnConfigStructure,
+    IColumnHeaderStructure,
+    ITotalList,
+    IFreezeProps,
+    IColumnConfig,
+    IPageSizes,
+    ISelected,
+    IColumnHeader,
+    IColumn,
+    IrowActions,
+    ILinksList,
+    ItemFields,
+    IFilterDataForRequest,
+  } from './Models/table.models'
+  import { StructureConfig } from './Models/table.enum'
+  
+  export {
+    Table,
+    IProps as ITableProps,
+    IColumnTotal,
+    IColumns,
+    IHeaders,
+    ITotals,
+    IColumnConfigStructure,
+    IColumnHeaderStructure,
+    ITotalList,
+    IFreezeProps,
+    IColumnConfig,
+    IPageSizes,
+    ISelected,
+    IColumnHeader,
+    IColumn,
+    IrowActions,
+    ILinksList,
+    StructureConfig,
+    ItemFields,
+    IFilterDataForRequest,
+  }
