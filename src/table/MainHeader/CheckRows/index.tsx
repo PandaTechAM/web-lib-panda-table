9a48d527970f8a939199eebbee44fbeea26df579
Samvel Baghdasaryan <samvel.baghdasaryan@pandatech.it>
2025-01-23 10:57:14 +0400

    select-dropdown

--- src/table/MainHeader/CheckRows/index.tsx
+++ src/table/MainHeader/CheckRows/index.tsx
@@ -1,8 +1,5 @@
-import React, { memo, useState } from 'react'
-import { CheckedItems } from '../../../Models/table.enum'
+import React, { memo } from 'react'
 import Checkbox from '../../../components/checkbox'
-import PopUp from '../../../components/popUp'
-import DropdownSvgIcon from '../../../svgIcons/DropdownSvgIcon'
 import './style.scss'
 interface ICheckRows<T extends Object> {
   data: T[]
@@ -13,103 +10,24 @@ interface ICheckRows<T extends Object> {
   checkAllDataFromDb(): void
   handleClose?: () => void
 }
-const dropdownStyle = {
-  height: '26px',
-  display: 'flex',
-  justifyContent: 'start',
-  padding: 0,
-  margin: 0,
-  translate: -10,
-  backgroundColor: 'white',
-  minWidth: 10,
-  border: 'none',
-}
-const CheckRows = <T extends Object>({
-  data,
-  checkedRows,
-  translations,
-  handleCheckAll,
-  unCheck,
-  checkAllDataFromDb,
-}: ICheckRows<T>) => {
-  const [selectedItem, setSelectedItem] = useState(2)
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
 
-  const [listForDropdown] = useState<Object[]>([
-    {
-      id: 2,
-      title: translations?.checkbox.allVisible || CheckedItems.SELECTED_VISIBLE,
-      action: (data?: T[]) => {
-        handleCheckAll(data)
-        setSelectedItem(1)
-      },
-    },
-    {
-      id: 3,
-      title: translations?.checkbox.none || CheckedItems.NONE,
-      action: () => {
-        unCheck()
-        setSelectedItem(2)
-      },
-    },
-  ])
-
+const CheckRows = <T extends Object>({ data, checkedRows, handleCheckAll, unCheck }: ICheckRows<T>) => {
   return (
-    <>
-      <div className='G-align-center G-check-all-component'>
-        <Checkbox
-          isCheck={data.length === checkedRows.length && checkedRows.length > 1}
-          onClick={
-            data.length === checkedRows.length
-              ? () => {
-                  unCheck()
-                  setSelectedItem(2)
-                }
-              : () => {
-                  handleCheckAll()
-                  setSelectedItem(1)
-                }
-          }
-          customClass='G-checkbox'
-        />
-      </div>
-      <div className='G-check-all-dropdown'>
-        <PopUp
-          ActiveIcon={DropdownSvgIcon}
-          style={dropdownStyle}
-          open={open}
-          anchorEl={anchorEl}
-          handleClick={handleClick}
-          handleClose={handleClose}
-        >
-          <ul className='G-dropdown-list'>
-            {listForDropdown.map((item: any, index: any) => {
-              return (
-                <li
-                  key={item.id}
-                  onClick={() => {
-                    item.action(data)
-                    handleClose()
-                  }}
-                  style={{
-                    color: index === selectedItem ? '#4844C5' : 'black',
-                  }}
-                >
-                  {item.title}
-                </li>
-              )
-            })}
-          </ul>
-        </PopUp>
-      </div>
-    </>
+    <div className='G-align-center G-check-all-component'>
+      <Checkbox
+        isCheck={data.length === checkedRows.length && checkedRows.length > 1}
+        onClick={
+          data.length === checkedRows.length
+            ? () => {
+                unCheck()
+              }
+            : () => {
+                handleCheckAll()
+              }
+        }
+        customClass='G-checkbox'
+      />
+    </div>
   )
 }
 
