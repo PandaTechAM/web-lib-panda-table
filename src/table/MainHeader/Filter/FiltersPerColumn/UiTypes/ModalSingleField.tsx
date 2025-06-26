4056dbe700e92e7b0b34f4bfddf73cabaea4f10c
Samvel Baghdasaryan <samvel.baghdasaryan@pandatech.it>
2025-06-26 12:27:13 +0400

    starts-with-ui

--- src/table/MainHeader/Filter/FiltersPerColumn/UiTypes/ModalSingleField.tsx
+++ src/table/MainHeader/Filter/FiltersPerColumn/UiTypes/ModalSingleField.tsx
@@ -123,15 +123,7 @@ const ModalForSingleField = ({
       val.length &&
       perColumnListForFilters?.length
     ) {
-      let newPerColumnList: string[] = perColumnListForFilters
-      if (!isLoadedMoreData) {
-        newPerColumnList = perColumnListForFilters ? [val, ...perColumnListForFilters] : [val]
-      }
-      if (perColumnListForFilters.length === 1) {
-        return perColumnListForFilters
-      } else {
-        return newPerColumnList
-      }
+      return perColumnListForFilters
     } else {
       return []
     }
@@ -190,11 +182,11 @@ const ModalForSingleField = ({
                     display: 'flex',
                     justifyContent: 'space-between',
                     minHeight: 40,
-                    borderBottom:
-                      props['data-option-index' as keyof typeof props] === 0 &&
-                      (perColumnListForFilters?.length ?? 0) > 1
-                        ? '1px solid #DCDCDC'
-                        : 'none',
+                    // borderBottom:
+                    //   props['data-option-index' as keyof typeof props] === 0 &&
+                    //   (perColumnListForFilters?.length ?? 0) > 1
+                    //     ? '1px solid #DCDCDC'
+                    //     : 'none',
                   }}
                 >
                   {item.ColumnName === columnName && isLoadingFilters ? (
